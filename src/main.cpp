eaa9633d776bb27b1c3f5078f047b630b8894e54
eosti <reidsox1@gmail.com>
2023-10-29 16:03:06 -0400

    fix shift register timing

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include <Arduino.h>
 #include "pins_arduino.h"
+#include <Arduino.h>
 #include <avr/interrupt.h>
 #include <avr/power.h>
 #include <avr/sleep.h>
 
-#define SHIFT_ROW_3 15
-#define SHIFT_ROW_2 14
-#define SHIFT_ROW_1 13
+#define SHIFT_ROW_3  15
+#define SHIFT_ROW_2  14
+#define SHIFT_ROW_1  13
 #define SHIFT_SSB_ON 11
-#define SHIFT_TX_PA 10
+#define SHIFT_TX_PA  10
 #define SHIFT_LNA_ON 9
 #define SHIFT_RED_EN 8
 #define SHIFT_LED_80 7
@@ -21,35 +21,37 @@
 #define SHIFT_LED_10 0
 
 // Order is pin 5, pin 6, for shift value 7, 6, 5
-#define SWITCH_TX_PA 5
-#define SWITCH_SSB_ON 4
-#define SWITCH_RX_LNA 3
-#define SWITCH_SET_PWR 2
+#define SWITCH_TX_PA    5
+#define SWITCH_SSB_ON   4
+#define SWITCH_RX_LNA   3
+#define SWITCH_SET_PWR  2
 #define SWITCH_SHOW_SWR 0
 
 // Pinout
-#define SPI_SCK_PIN 13  // PB5: Connect to PA pin 10, Clock
-#define SPI_MOSI_PIN 11 // PB3: Connect to PA pin 9, Data In
-#define SPI_SS_PIN 10   // PB2: Connect to PA pin 7, OE_n
+#define SPI_SCK_PIN     13 // PB5: Connect to PA pin 10, Clock
+#define SPI_MOSI_PIN    11 // PB3: Connect to PA pin 9, Data In
+#define SPI_SS_PIN      10 // PB2: Connect to PA pin 7, OE_n
 
 #define SHIFT_VALID_PIN 8 // PB0: Connect to PA pin 8, Storage Clock
-#define MATRIX_A_PIN 7    // PD7: Connect to PA pin 5, MAT_A
-#define MATRIX_B_PIN 6    // PD6: Connect to PA pin 6, MAT_B
+#define MATRIX_A_PIN    7 // PD7: Connect to PA pin 5, MAT_A
+#define MATRIX_B_PIN    6 // PD6: Connect to PA pin 6, MAT_B
 
-#define DA_EN_PIN 5  // Connect to DA ??? via 3.3V voltage divider
-#define DA_SWR_PIN 4 // Connect to DA ???
+#define DA_EN_PIN       5 // Connect to DA ??? via 3.3V voltage divider
+#define DA_SWR_PIN      4 // Connect to DA ???
 
 // Globals
-volatile uint8_t spi_buffer[2] = {0};
-volatile bool spi_pos = 0;
-volatile bool message_ready = 0;
+volatile uint8_t spi_buffer[2]  = {0};
+volatile bool spi_pos           = 0;
+volatile uint8_t port_d_preload = 0;
+
 
 volatile uint16_t curShiftState = 0;
 volatile uint8_t curSwitchState = 0;
+volatile uint8_t singleShotSwitch = 0;
 
-char commandByte = 0;
-bool daSwitch = 0;
-bool daOverSWR = 0;
+char commandByte                = 0;
+bool daSwitch                   = 0;
+bool daOverSWR                  = 0;
 
 void processCommand(char command, char target);
 
@@ -58,11 +60,44 @@ void processCommand(char command, char target);
  *  Fires when SPIF bit is set (i.e. there is data available)
  *  ISR stores the byte available into a buffer.
  */
-ISR(SPI_STC_vect)
-{
+ISR(SPI_STC_vect) {
+    // To prevent carryover to next row
+    // PORTD = port_d_preload;
+
+    PORTD = port_d_preload;
+
     spi_buffer[spi_pos] = SPDR;
 
-    spi_pos = spi_pos ? 0 : 1;
+    spi_pos             = spi_pos ? 0 : 1;
+
+    if (spi_pos) {
+        port_d_preload = (PORTD & 0b00111111);
+    } else {
+        port_d_preload = PORTD;
+    }
+
+    // Post-processing
+    if (!spi_pos) {
+        curShiftState = (((uint16_t)spi_buffer[0]) << 8 | spi_buffer[1]);
+
+        // Set switch col state to match row state
+        // col pins are bit 7 = MAT_A, bit 6 = MAT_B
+        // Serial.print(curShiftState, HEX);
+        // Serial.println(bitRead(curShiftState, SHIFT_ROW_1));
+        uint8_t writeValues = 0;
+        if (bitRead(curShiftState, SHIFT_ROW_3)) {
+            writeValues = ((curSwitchState | singleShotSwitch) & 0b00110000) << 2;
+            if (singleShotSwitch & 0b00110000) singleShotSwitch = 0;
+        } else if (bitRead(curShiftState, SHIFT_ROW_2)) {
+            writeValues = ((curSwitchState | singleShotSwitch) & 0b00001100) << 4;
+            if (singleShotSwitch & 0b00001100) singleShotSwitch = 0;
+        } else {
+            writeValues = ((curSwitchState | singleShotSwitch) & 0b00000011) << 6;
+            if (singleShotSwitch & 0b00000011) singleShotSwitch = 0;
+        }
+
+        PORTD = (PORTD & 0b00111111) | writeValues;
+    }
 }
 
 /*
@@ -73,34 +108,13 @@ ISR(SPI_STC_vect)
  */
 
 ISR(PCINT0_vect) {
-
-    // if ((PINB & (1 << PINB0)) == 1)
-    // The valid time is like 100ns which is faster than the Uno can get to the ISR
-    if (spi_pos == 0)
-    {
-        curShiftState = (((uint16_t)spi_buffer[0]) << 8 | spi_buffer[1]);
-
-        message_ready = 1;
-
-        // Set switch col state to match row state
-        // col pins are bit 7 = MAT_A, bit 6 = MAT_B
-        //Serial.print(curShiftState, HEX);
-        //Serial.println(bitRead(curShiftState, SHIFT_ROW_1));
-        uint8_t writeValues = 0;
-        if (bitRead(curShiftState, SHIFT_ROW_3)) {
-            writeValues = (curSwitchState & 0b00110000) << 2;
-        } else if (bitRead(curShiftState, SHIFT_ROW_2)) {
-            writeValues = (curSwitchState & 0b00001100) << 4;
-        } else {
-            writeValues = (curSwitchState & 0b00000011) << 6;
-        }
-
-        PORTD = (PORTD & 0b00111111) | writeValues;
+    // Resync the buffer
+    if (spi_pos == 1) {
+        spi_pos = 0;
     }
 }
 
-void setup()
-{
+void setup() {
     // General I/O mapping
     pinMode(SHIFT_VALID_PIN, INPUT);
     pinMode(MATRIX_A_PIN, OUTPUT);
@@ -118,19 +132,17 @@ void setup()
     // Enable interrupt on "shift valid" rising edge
     // Assuming SHIFT_VALID_PIN is pin 8 = PCINT0
     // Source : https://thewanderingengineer.com/2014/08/11/arduino-pin-change-interrupts/
-    PCICR |= _BV(PCIE0);   // turn on port b
-    PCMSK0 |= _BV(PCINT0); // turn on pin PB0, which is PCINT0, arduino pin 8
+    PCICR |= _BV(PCIE0);   // turn on port b interrupts
+    PCMSK0 |= _BV(PCINT0); // turn on pin PB0 interrupts, which is PCINT0, arduino pin 8
     PCIFR |= _BV(PCIF0);   // Clear outstanding interrupts
     sei();                 // enable interrupts
 
     power_timer0_disable();
     Serial.begin(115200);
     Serial.println("Toptek Switch Interface");
-
 }
 
-void loop()
-{
+void loop() {
 
     // Switch state handling is automatically handled by the ISR
     // In the master loop we just have to poll for new data and serial activity
@@ -147,24 +159,23 @@ void loop()
             processCommand(commandByte, peek);
             commandByte = 0;
         }
-
     }
 
     // Refresh DA values
     digitalWrite(DA_EN_PIN, daSwitch);
-    daOverSWR = (DA_SWR_PIN);
+    daOverSWR = digitalRead(DA_SWR_PIN);
 }
 
 void processCommand(char command, char target) {
     Serial.println("");
     if (command == 'R') {
         uint16_t curShiftState_buf = curShiftState;
-        uint16_t curSwitchState_buf = curSwitchState;
-        switch(target) {
+        uint8_t curSwitchState_buf = curSwitchState;
+        switch (target) {
             case 'A':
                 Serial.println(curShiftState_buf, HEX);
                 break;
-            case 'S': 
+            case 'S':
                 Serial.println(daOverSWR, HEX);
                 break;
             case 'W':
@@ -193,7 +204,7 @@ void processCommand(char command, char target) {
         }
     } else if (command == 'S') {
         uint8_t curSwitchState_buf = curSwitchState;
-        switch(target) {
+        switch (target) {
             case '1':
                 bitSet(curSwitchState_buf, SWITCH_SSB_ON);
                 break;
@@ -209,7 +220,7 @@ void processCommand(char command, char target) {
             case '5':
                 bitSet(curSwitchState_buf, SWITCH_SHOW_SWR);
                 break;
-            case '6': 
+            case '6':
                 daSwitch = 1;
                 break;
             default:
@@ -218,7 +229,7 @@ void processCommand(char command, char target) {
         curSwitchState = curSwitchState_buf;
     } else if (command == 'U') {
         uint8_t curSwitchState_buf = curSwitchState;
-        switch(target) {
+        switch (target) {
             case '1':
                 bitClear(curSwitchState_buf, SWITCH_SSB_ON);
                 break;
@@ -234,13 +245,33 @@ void processCommand(char command, char target) {
             case '5':
                 bitClear(curSwitchState_buf, SWITCH_SHOW_SWR);
                 break;
-            case '6': 
+            case '6':
                 daSwitch = 0;
                 break;
             default:
                 Serial.println("Unhandled statement");
         }
         curSwitchState = curSwitchState_buf;
+    } else if (command == 'P') {
+        switch (target) {
+            case '1':
+                bitSet(singleShotSwitch, SWITCH_SSB_ON);
+                break;
+            case '2':
+                bitSet(singleShotSwitch, SWITCH_TX_PA);
+                break;
+            case '3':
+                bitSet(singleShotSwitch, SWITCH_SET_PWR);
+                break;
+            case '4':
+                bitSet(singleShotSwitch, SWITCH_RX_LNA);
+                break;
+            case '5':
+                bitSet(singleShotSwitch, SWITCH_SHOW_SWR);
+                break;
+            default:
+                Serial.println("Unhandled statement");
+        }
     } else if (command == 'E' && target == 'N') {
         pinMode(MATRIX_A_PIN, OUTPUT);
         pinMode(MATRIX_B_PIN, OUTPUT);
